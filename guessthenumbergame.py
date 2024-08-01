82f30bab0bb8ef4f4b13da48b03c281d63fea425
Jashan <jashanmalhotra3768@gmail.com>
2024-08-02 00:03:15 +0530

    Update guessthenumbergame.py

--- guessthenumbergame.py
+++ guessthenumbergame.py
@@ -2,21 +2,30 @@ import random
 def number():
     print("Welcome to the number guessing game:")
     input("Press enter to continue.....")
-    guess=0
+    guess=int(input("Number of tries:"))
     f=int(input("Enter the lower limit:"))
     b=int(input("Enter the upper limit:"))
     a=random.randint(f,b)
     while True:
             print("Hint: Your number would lie between",f,"and",b)
             j=int(input("Enter your number :"))
-            guess +=1
+            guess -=1
+            if guess ==0:
+                print("GAME OVER XD")
+                break
             if j < a :
                 print("Your option number is smaller")
-                f=0+j
+                print(guess, "tries left")
+                if f<j:
+                    f=0+j
             elif j>a:
                 print("Your option is bigger number")
-                b=0+j
+                print(guess, "tries left")
+                if b>j:
+                    b=0+j
             else :
-                print("Congratulation!! You found the number",a,"in",guess,"tries")
+                
+                print("Congratulation!! You found the number",a)
                 exit()
+            
 number()
